b0e0bdaa7355d1feb52a51d934ed2a32ec8e6e72
rakib-siddiki <rmrakib901@gmail.com>
2024-05-12 01:18:40 -0700

    fix: deploy fetch

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,5 +1,6 @@
 export const API_BASE_URL =
-    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api';
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    'https://front-end-task-zone-sparks-limited.vercel.app/api';
 export const SITE_DOMAIN =
     process.env.NEXT_PUBLIC_SITE_DOMAIN || 'http://localhost:3000/';
 export const MONGODB_URI =
